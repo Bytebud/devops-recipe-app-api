68d803e5d245cf0ae3c4e515336f6e763059766c
Sravan Kumar <sky.sravan@gmail.com>
2025-07-28 04:22:00 +0530

    fixed error- added rds instance

--- infra/deploy/database.tf
+++ infra/deploy/database.tf
@@ -3,7 +3,7 @@
 ############
 
 resource "aws_db_subnet_group" "main" {
-  name       = "${loacal.prefix}-main"
+  name = "${local.prefix}-main"
   subnet_ids = [
     aws_subnet.private_a.id,
     aws_subnet.private_b.id,
@@ -20,32 +20,32 @@ resource "aws_security_group" "rds" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    protocol    = "tcp"
-    from_port   = 5432
-    to_port     = 5432
+    protocol  = "tcp"
+    from_port = 5432
+    to_port   = 5432
   }
 
-  tags ={
+  tags = {
     Name = "${local.prefix}-db-security-group"
   }
 }
 
 resource "aws_db_instance" "main" {
-  identifier              = "${local.prefix}-db"
-  db_name                 = "recipe"
-  allocated_storage       = 20
-  storage_type            = "gp2"
-  engine                  = "postgres"
-  engine_version          = "15.3"
+  identifier                 = "${local.prefix}-db"
+  db_name                    = "recipe"
+  allocated_storage          = 20
+  storage_type               = "gp2"
+  engine                     = "postgres"
+  engine_version             = "15.3"
   auto_minor_version_upgrade = true
-  instance_class          = "db.t4g.micro"
-  username                = var.db_username
-  password                = var.db_password
-  skip_final_snapshot     = true
-  db_subnet_group_name    = aws_db_subnet_group.main.name
-  multi_az = false
-  backup_retention_period = 0
-  vpc_security_group_ids  = [aws_security_group.rds.id]
+  instance_class             = "db.t4g.micro"
+  username                   = var.db_username
+  password                   = var.db_password
+  skip_final_snapshot        = true
+  db_subnet_group_name       = aws_db_subnet_group.main.name
+  multi_az                   = false
+  backup_retention_period    = 0
+  vpc_security_group_ids     = [aws_security_group.rds.id]
 
   tags = {
     Name = "${local.prefix}-db-instance"
