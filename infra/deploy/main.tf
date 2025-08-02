978dc57b48fe54a3361ba2326cb733c8552f04a7
Sravan Kumar <sky.sravan@gmail.com>
2025-08-03 01:55:29 +0530

    changed s3 key

--- infra/deploy/main.tf
+++ infra/deploy/main.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket               = "devops-recipe-app-tf-state-30052025"
-    key                  = "tf-state-deploy"
+    key                  = "tf-state-deploy-env"
     workspace_key_prefix = "tf-state-deploy-env"
     region               = "us-east-1"
     encrypt              = true
